29e372bb12265a07b215ea66e6a6029668c6afab
Multi <MultHub@users.noreply.github.com>
2016-03-17 18:05:39 +0100

    APP-GET! PLS WAIT
    IT'S WIP BUT WILL BE A CRAPPY APP STORE
    MIGHT EVEN ADD STRAFE *cough*

---
+++ appsInstaller.lua
@@ -0,0 +1,16 @@
+local function wget(url, path)
+	local r = http.get(url)
+	if not r then
+		return false
+	end
+	local f = fs.open(path, "w")
+	f.write(r.readAll())
+	f.close()
+	r.close()
+	return true
+end
+
+local base = "https://raw.github.com/MultHub/Aurora-apps/app-get/master/app-get"
+
+wget(base .. "/aInfo", "apps/app-get/aInfo")
+wget(base .. "/app-get", "apps/app-get/app-get")
